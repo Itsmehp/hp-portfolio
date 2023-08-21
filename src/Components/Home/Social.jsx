b3f5bef43132471df7e68a2ac8cb91350411c778
DESKTOP-L9U2CF5\HARR <hpzhd.hp@gmail.com>
2023-08-21 21:12:52 +0530

    First Commit of Portfolio (added NavBar)

---
+++ src/Components/Home/Social.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a
+        href="https://www.instagram.com/hey.its.hp/"
+        className="home__social-icon"
+      >
+        <i class="uil uil-apps"></i>
+      </a>
+      <a href="https://github.com/Itsmehp" className="home__social-icon">
+        <i class="uil uil-github"></i>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/harshil-patel-027715211/"
+        className="home__social-icon"
+      >
+        <i class="uil uil-linkedin-alt"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
