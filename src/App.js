d21a214c0e81eb30fd357e736e581a540758572a
DESKTOP-L9U2CF5\HARR <hpzhd.hp@gmail.com>
2023-10-15 19:30:03 +0530

    bug fix for preloader removal

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Contact from "./Components/contact/Contact";
 import Footer from "./Components/footer/Footer";
 import ScrollUp from "./Components/scrollup/ScrollUp";
 import Work from "./Components/work/Work";
-import PreLoader from "./Components/preloader/Preloader";
+// import PreLoader from "./Components/preloader/Preloader";
 
 function App() {
   return (
