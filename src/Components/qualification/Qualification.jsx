098ccd29e52bbac02655efea5457384f4e5e56a0
Itsmehp <harshil.hpz@gmail.com>
2025-03-10 13:02:13 +0100

    Qualification changes

--- src/Components/qualification/Qualification.jsx
+++ src/Components/qualification/Qualification.jsx
@@ -1,13 +1,7 @@
 import React from "react";
 import "./qualification.css";
-import { useState } from "react";
 
 const Qualification = () => {
-  const [toggleState, setToggleState] = useState(0);
-
-  const ToggleTab = (index) => {
-    setToggleState(index);
-  };
   return (
     <section className="qualification section">
       <h2 className="section__title">Qualification</h2>
@@ -15,40 +9,117 @@ const Qualification = () => {
 
       <div className="qualification__container container">
         <div className="qualification__tabs">
-          <div
-            className={
-              toggleState === 1
-                ? "qualification__button  qualification__active button--flex"
-                : "qualification__button button--flex"
-            }
-            onClick={() => ToggleTab(1)}
-          >
-            <i className="uil uil-graduation-cap qualification__icon"></i>
-            Education
-          </div>
-
-          <div
-            className={
-              toggleState === 2
-                ? "qualification__button  qualification__active button--flex"
-                : "qualification__button button--flex"
-            }
-            onClick={() => ToggleTab(2)}
-          >
+          <div className="qualification__button qualification__active button--flex">
+          <i className="uil uil-graduation-cap qualification__icon"></i>
+            Timeline
             <i className="uil uil-briefcase-alt qualification__icon"></i>
-            Experience
           </div>
         </div>
 
         <div className="qualification__sections">
-          <div
-            className={
-              toggleState === 1
-                ? "qualification__content qualification__content-active"
-                : "qualification__content"
-            }
-          >
+          <div className="qualification__content qualification__content-active">
+          
+          <div className="qualification__data">
+              
+              <div>
+                <h3 className="qualification__title">
+                  Masters in Artificial Intellegence and Robotics
+                </h3>
+                <span className="qualification__subtitle">
+                <span className="education-badge">Hochschule Hof, Germany</span>
+                </span>
+                <div className="qualification__calendar">
+                  <i className="uil uil-calendar-alt"></i>
+                  2024 - Present
+                </div>
+              </div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+
             <div className="qualification__data">
+            <div></div>
+            <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+              <div>
+                <h3 className="qualification__title">
+                  Freelancing Web Developer
+                </h3>
+                <span className="qualification__subtitle">Online</span>
+                <div className="qualification__calendar">
+                  <i className="uil uil-calendar-alt"></i>
+                  2023 - 2024
+                </div>
+              </div>
+              
+            </div>
+
+
+
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">Front/Back-End Developer (ReactJS)</h3>
+                <span className="qualification__subtitle">
+                HP Infosys - India
+                </span>
+                <div className="qualification__calendar">
+                  <i className="uil uil-calendar-alt"></i>
+                  2022 - 2023
+                </div>
+              </div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+
+
+            <div className="qualification__data">
+              <div></div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+              <div>
+                <h3 className="qualification__title">
+                  Web Development / Design (ReactJS)
+                </h3>
+                <span className="qualification__subtitle">Udemy</span>
+                <div className="qualification__calendar">
+                  <i className="uil uil-calendar-alt"></i>
+                  2021-2022
+                </div>
+              </div>
+
+            </div>
+
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">HTML CSS & JavaScript</h3>
+                <span className="qualification__subtitle">
+                  Front-End Intern
+                </span>
+                <div className="qualification__calendar">
+                  <i className="uil uil-calendar-alt"></i>
+                  2021-2022
+                </div>
+              </div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              <div></div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
               <div>
                 <h3 className="qualification__title">
                   Bachelors in Computer Science
@@ -60,109 +131,6 @@ const Qualification = () => {
                   <i className="uil uil-calendar-alt"></i>
                   2017 - 2021
                 </div>
-              </div>
-              <div>
-                <span className="qualification__rounder"></span>
-                <span className="qualification__line"></span>
-              </div>
-            </div>
-
-            <div className="qualification__data">
-              <div></div>
-              <div>
-                <span className="qualification__rounder"></span>
-                <span className="qualification__line"></span>
-              </div>
-              <div>
-                <h3 className="qualification__title">HTML CSS & JavaScript</h3>
-                <span className="qualification__subtitle">
-                  Front-End Intern
-                </span>
-                <div className="qualification__calendar">
-                  <i className="uil uil-calendar-alt"></i>
-                  2021-2022
-                </div>
-              </div>
-            </div>
-
-            <div className="qualification__data">
-              <div>
-                <h3 className="qualification__title">
-                  Web Development / Design (ReactJS)
-                </h3>
-                <span className="qualification__subtitle">Udemy</span>
-                <div className="qualification__calendar">
-                  <i className="uil uil-calendar-alt"></i>
-                  2021-2022
-                </div>
-              </div>
-              <div>
-                <span className="qualification__rounder"></span>
-                <span className="qualification__line"></span>
-              </div>
-            </div>
-          </div>
-
-          <div
-            className={
-              toggleState === 2
-                ? "qualification__content qualification__content-active"
-                : "qualification__content"
-            }
-          >
-            <div className="qualification__data">
-              <div>
-                <h3 className="qualification__title">
-                  Front-End Web Developer (HTML, CSS, JavaScript)
-                </h3>
-                <span className="qualification__subtitle">
-                  Alite Projects - India
-                </span>
-                <div className="qualification__calendar">
-                  <i className="uil uil-calendar-alt"></i>
-                  2022 - 2023
-                </div>
-              </div>
-              <div>
-                <span className="qualification__rounder"></span>
-                <span className="qualification__line"></span>
-              </div>
-            </div>
-
-            <div className="qualification__data">
-              <div></div>
-              <div>
-                <span className="qualification__rounder"></span>
-                <span className="qualification__line"></span>
-              </div>
-              <div>
-                <h3 className="qualification__title">
-                  Front/Back-End Developer (ReactJS)
-                </h3>
-                <span className="qualification__subtitle">
-                  HP Infosys - India
-                </span>
-                <div className="qualification__calendar">
-                  <i className="uil uil-calendar-alt"></i>
-                  2022 - 2023
-                </div>
-              </div>
-            </div>
-
-            <div className="qualification__data">
-              <div>
-                <h3 className="qualification__title">
-                  Freelancing Web Developer
-                </h3>
-                <span className="qualification__subtitle">Online</span>
-                <div className="qualification__calendar">
-                  <i className="uil uil-calendar-alt"></i>
-                  2023 - Present
-                </div>
-              </div>
-              <div>
-                <span className="qualification__rounder"></span>
-                <span className="qualification__line"></span>
               </div>
             </div>
           </div>
